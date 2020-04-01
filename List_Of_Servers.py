b6696e4aa03003a364d2a08bab059eb401c7fbcf
villtord <villtord@gmail.com>
2020-04-01 13:38:31 +0200

    All server parameters defined in a separate file

--- List_Of_Servers.py
+++ List_Of_Servers.py
@@ -1,6 +1,7 @@
 """
 List of servers for ARPES computer (AC computer)
+Structure: {NAME:[Port, Driver, COM-port,pressure_index in received string,color of the display]}
 """
 import Driver_Pfeiffer_TPG261
 
-server_list = {"OXICHAMBER":[63207, Driver_Pfeiffer_TPG261, 'COM22']}
+server_list = {"OXICHAMBER":[63207, Driver_Pfeiffer_TPG261, 192.168.'COM22',0,"blue"]}
