7e06cec5818c2546136a04d8e6dc6e1edbd47c23
Villtord <Villtord@users.noreply.github.com>
2020-04-09 17:33:26 +0200

    Added specific drivers StartNewServer.py can now transmit a command to the device (useful for Lakeshore) TODO: start only one server if dict.items differ only in channel!!!

--- Driver_Leybold.py
+++ Driver_Leybold.py
@@ -1,5 +1,5 @@
 """
-Driver for Pfeiffer TPG261 pressure control device.
+Driver for Leybold Combivac IT23 pressure control device.
 The latter must be connected through a certain COM port (virtual or real).
 Driver first initializes, then opens serial connection and reads the pressure values.
 Driver handles the correct pressure to transmit
@@ -12,50 +12,45 @@ import io
 class Driver:
     def __init__(self, com_name):
         self.com_name = com_name
-        self.get_pressure("COM,0")   # request pressure values each 100 ms
+        self.pressure_value = 0.0
 
     def get_pressure(self, *args: "optional command"):
         """ Opens serial connection and request/read the pressure values    """
         ser = serial.Serial(self.com_name,
                             baudrate=9600,
                             bytesize=serial.EIGHTBITS,
-                            stopbits=serial.STOPBITS_ONE,
                             parity=serial.PARITY_NONE,
-                            timeout=0.5)
+                            stopbits=serial.STOPBITS_ONE,
+                            timeout=0.1)
         ser_io = io.TextIOWrapper(io.BufferedRWPair(ser, ser, 1),
-                                  newline='\r\n',
+                                  newline='\r',
                                   line_buffering=True)
         """Write a command(s) to the controller and read the reply """
         if args:
             try:
-                command_full = str(args[0]) + "\r\n"
+                command_full = str(args[0]) + "\r"
                 ser_io.write(command_full)
                 self.read_str_raw = ser_io.readline()
             except Exception as e:
                 logging.exception(e)
                 pass
         else:
-            self.read_str_raw = ser_io.readline()
+            command_full = "MES 1" + "\r"
+            ser_io.write(command_full)  # request pressure value for channel 1
+            self.read_str_raw = ser_io.readline()[7:]  # get only necessary numbers
+            command_full = "MES 2" + "\r"
+            ser_io.write(command_full)  # # request pressure value for channel 2
+            self.read_str_raw += ","+ser_io.readline()[7:]  # get only necessary numbers
         ser.close()
         """Handle the received data and decide what to transmit"""
         try:
-            self.gauge_status = int(self.read_str_raw.split(',')[0])
+            self.pressure_value = ''.join(self.read_str.split())  # get rid of spaces
         except:
-            self.gauge_status = 5
+            self.pressure_value = "NAN"
             pass
+
+        self.read_str = self.pressure_value
         try:
-            self.pressure_value = float(self.read_str_raw.split(',')[1])
-        except:
-            self.pressure_value = 0.0
-            pass
-        if (self.gauge_status == 0) and (self.pressure_value < 0.01):
-            self.read_str = self.pressure_value
-        elif (self.gauge_status == 1):
-            self.read_str = 0.0  # underrange
-        elif (self.gauge_status == 2):
-            self.read_str = 0.01  # overrange
-        try:
-            # self.read_str = str("{:.10f}".format(float(0.000001)))
             return str(self.read_str)
         except Exception as e:
             logging.exception(e)
