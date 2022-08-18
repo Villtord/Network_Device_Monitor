d0c6c388e6d3e714a73e4874c08223267892ee8d
eir17846 <victor.rogalev@diamond.ac.uk>
2022-08-18 17:41:54 +0100

    sys.path changed to point one folder up!

--- docs/conf.py
+++ docs/conf.py
@@ -18,15 +18,10 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('source'))
+sys.path.insert(0, os.path.abspath('..'))  # Source code dir relative to this file
 
 
 # -- General configuration ------------------------------------------------
-
-# If your documentation needs a minimal Sphinx version, state it here.
-#
-# needs_sphinx = '1.0'
-
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
