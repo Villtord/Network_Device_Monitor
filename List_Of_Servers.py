65fcaec2247b58cbaf581679f256cd94853d77a7
eir17846 <victor.rogalev@diamond.ac.uk>
2022-08-18 15:32:54 +0100

    Changes to build docs

--- List_Of_Servers.py
+++ List_Of_Servers.py
@@ -5,23 +5,14 @@ Structure:
 """
 import socket  # temporary
 
+temporary_ip = socket.gethostbyname(socket.gethostname())
 
 server_list = dict(
-                    OXIDATION_CHAMBER=["132.187.37.21", 63207, 'COM22', "Driver_Pfeiffer_TPG261", 0, "blue"],
-                   LAKESHORE_T_A=["132.187.37.21", 63205, 'COM20', "Driver_Lakeshore", 0, "orange"],
-                   ANALYSIS_CHAMBER=["132.187.37.41", 63202, 'COM104', "Driver_AML", 0, "green"],
-                   PREPARATION_CHAMBER=["132.187.37.41", 63200, 'COM99', "Driver_Agilent", 1, "red"],
-                   DISTRIBUTION_CHAMBER=["132.187.37.41", 63206, 'COM105', "Driver_Pfeiffer_TPG261", 0, "darkviolet"],
-                   HE_CHAMBER=["132.187.37.41", 63201, 'COM106', "Driver_Leybold", 1, "white"],
-                   LOAD_LOCK_CHAMBER=["132.187.37.41", 63201, 'COM106', "Driver_Leybold", 0, "orange"],
+                    OXIDATION_CHAMBER=[temporary_ip, 63207, 'COM22', "Driver_Pfeiffer_TPG261", 0, "blue"],
+                    LAKESHORE_T_A=[temporary_ip, 63205, 'COM20', "Driver_Lakeshore", 0, "orange"],
+                    ANALYSIS_CHAMBER=[temporary_ip, 63202, 'COM104', "Driver_AML", 0, "green"],
+                    PREPARATION_CHAMBER=[temporary_ip, 63200, 'COM99', "Driver_Agilent", 1, "red"],
+                    DISTRIBUTION_CHAMBER=[temporary_ip, 63206, 'COM105', "Driver_Pfeiffer_TPG261", 0, "darkviolet"],
+                    HE_CHAMBER=[temporary_ip, 63201, 'COM106', "Driver_Leybold", 1, "white"],
+                    LOAD_LOCK_CHAMBER=[temporary_ip, 63201, 'COM106', "Driver_Leybold", 0, "orange"],
                    )
-
-# temporary_ip = socket.gethostbyname(socket.gethostname()) # temporary
-#
-# server_list = dict(OXIDATION_CHAMBER=["132.187.37.21", 63207, 'COM22', "Driver_Pfeiffer_TPG261", 0, "blue"],
-#                    LAKESHORE_T_A=["132.187.37.21", 63205, 'COM20', "Driver_Lakeshore", 0, "orange"],
-#                    ANALYSIS_CHAMBER=["132.187.37.41", 63202, 'COM104', "Driver_AML", 0, "green"],
-#                    PREPARATION_CHAMBER=[temporary_ip, 63200, 'COM99', "Driver_Agilent", 0, "red"],
-#                    DISTRIBUTION_CHAMBER=[temporary_ip, 63206, 'COM105', "Driver_Pfeiffer_TPG261", 0, "darkviolet"],
-#                    HE_CHAMBER=[temporary_ip, 63201, 'COM106', "Driver_Leybold", 1, "white"],
-#                    LOAD_LOCK_CHAMBER=[temporary_ip, 63201, 'COM106', "Driver_Leybold", 0, "orange"],)
--- docs/README.rst
+++ docs/README.rst
@@ -2,4 +2,4 @@
 Monitor, serve and log devices connected through the serial COM ports
 List of IPs and COM-ports is defined in list_of_servers.py
 
-Originally developed for EP4 ARPES lab in Wuerzburg Uni
+Originally developed for EP4 ARPES lab in Wuerzburg University
